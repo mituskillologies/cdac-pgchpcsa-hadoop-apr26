dab2bc16a5e4d8fc6d84f3f15ec1442449ac8b18
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2026-04-07 10:07:15 +0530

    Add files via upload

---
+++ 01 Java Programs/Variables.java
@@ -0,0 +1,10 @@
+class Variables {
+	public static void main(String [] args) {
+		int num = 56;    			// Declare the variable
+		float data = 12.90f;
+		String name = "Tushar";
+		System.out.println("Number: " + num);
+		System.out.println("Decimal Number: " + data);
+		System.out.println("Name: " + name);
+	}
+}
