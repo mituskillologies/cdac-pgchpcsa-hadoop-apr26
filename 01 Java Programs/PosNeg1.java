ec802f11762f887369d026dc2e05724babc17915
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2026-04-09 11:40:55 +0530

    Add files via upload

---
+++ 01 Java Programs/PosNeg1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class PosNeg1 {
+	public static void main(String [] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+		int num = in.nextInt();
+		if (num > 0) 
+			System.out.println(num + " is positive!");
+		else if (num == 0)
+			System.out.println("Number is ZERO!");
+		else
+			System.out.println(num + " is negative!");
+	}
+}
