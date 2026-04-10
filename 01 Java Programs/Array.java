37fc33e481ec07cc5fd6715bc868de3cf4b49c9b
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2026-04-10 11:45:14 +0530

    Add files via upload

---
+++ 01 Java Programs/Array.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Array {
+	public static void main(String [] args) {
+		int num1[] = new int[10];
+		int num2[] = {45,11,24,38,41,29};
+		
+		System.out.println("Element-0: " + num2[0]);
+		System.out.println("Total: " + num2.length);
+	}
+}
