7e2b2df750203ae328be73c7b74f17829ad6925b
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2026-04-10 11:45:32 +0530

    Add files via upload

---
+++ 01 Java Programs/While.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class While {
+	public static void main(String [] args) {
+		int num = 0;
+		while (num < 10) {
+			System.out.println("Tushar");
+			num += 1;
+		}
+	}
+}
