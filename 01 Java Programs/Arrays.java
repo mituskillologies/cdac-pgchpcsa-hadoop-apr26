fc929e1917f773ace39d33b7e46aee1072205963
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2026-04-12 13:49:05 +0530

    Add files via upload

---
+++ 01 Java Programs/Arrays.java
@@ -0,0 +1,13 @@
+// Addition of 10 natural numbers using for loop.
+class Arrays {
+	public static void main(String arg[]) {
+		int num[] = {61,59,32,45,92};
+		int num1[] = new int[10];
+		
+		for(int i = 0; i < num.length; i++)
+			System.out.println(num[i]);
+			
+		num1[3] = 78;
+		num1[6] = 67;
+	}
+}
