c9106b3a3c452b59fccbee9d40c08cb6140795e9
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2026-04-14 13:31:15 +0530

    Add files via upload

---
+++ 01 Java Programs/Break.java
@@ -0,0 +1,12 @@
+class Break {
+	public static void main(String arg[]) {
+		int i = 0;
+		
+		while ( i < 10) {               
+			if(i == 5)
+				break;
+			System.out.println(i);
+			i++;		
+		}
+	}
+}
