1a98e145e36ec4e46ba080f2b86bb1f7e9aef742
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2026-04-15 11:22:03 +0530

    Add files via upload

---
+++ 01 Java Programs/Decisions.java
@@ -0,0 +1,22 @@
+// Write the program to check the number is positive / negative
+/*
+	Date: 4/4/2026
+*/
+
+// The if - else statement
+// Relational Operators
+
+class Decisions {
+	public static void main(String arg[]) {
+		int num = 67;
+		
+		if (num > 0) {
+			System.out.println("Number is positive!");
+		}
+		else {
+			System.out.println("Number is Negative!");
+		}		
+		
+		System.out.println("Program Ended!");
+	}
+}
