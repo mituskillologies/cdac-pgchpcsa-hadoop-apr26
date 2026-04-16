018ebd8ce97f6e7d190dc5f4c916273d4cb0aab5
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2026-04-16 16:22:42 +0530

    Add files via upload

---
+++ 01 Java Programs/ForAdd.java
@@ -0,0 +1,10 @@
+// Addition of 10 natural numbers using for loop.
+class ForAdd {
+	public static void main(String arg[]) {
+		int add = 0; 
+		for(int num = 1; num <= 10; num++) {
+			add += num;
+		}
+		System.out.println(add);
+	}
+}
