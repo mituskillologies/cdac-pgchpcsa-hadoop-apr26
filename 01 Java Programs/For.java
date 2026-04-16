a064657c1c80987b7f75609c6b90b6239924f2e9
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2026-04-16 16:22:31 +0530

    Add files via upload

--- 01 Java Programs/For.java
+++ 01 Java Programs/For.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
+
 class For {
-	public static void main(String [] args) {
-		for (int num = 0 ; num < 10 ; num += 1) {
-			System.out.println("Tushar");
+	public static void main(String arg[]) {
+		for(int num = 0; num <= 10; num++) {
+			System.out.println(num);
 		}
 	}
 }
