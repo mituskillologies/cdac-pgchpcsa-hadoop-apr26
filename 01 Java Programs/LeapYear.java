eed3ca5804da9a8dc8ae202bc35cc142a76c939a
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2026-04-19 10:23:33 +0530

    Add files via upload

---
+++ 01 Java Programs/LeapYear.java
@@ -0,0 +1,22 @@
+// Write the program to check the number is odd / even
+/*
+	Date: 4/4/2026
+*/
+
+// The if - else statement
+// Relational Operators
+
+class LeapYear {
+	public static void main(String arg[]) {
+		int year = 2026;
+		
+		if (num % 2 == 0) {
+			System.out.println("Number is Even!");
+		}
+		else {
+			System.out.println("Number is Odd!");
+		}		
+		
+		System.out.println("Program Ended!");
+	}
+}
