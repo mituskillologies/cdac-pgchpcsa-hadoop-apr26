f81e4ebf6e52a701505c3a8e330ef8effdadc278
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2026-04-19 10:23:57 +0530

    Add files via upload

---
+++ 01 Java Programs/OddEven.java
@@ -0,0 +1,22 @@
+// Write the program to check the number is odd / even
+/*
+	Date: 4/4/2026
+*/
+
+// The if - else statement
+// Relational Operators
+
+class OddEven {
+	public static void main(String arg[]) {
+		int num = 68;
+		
+		if (num % 2 == 0) {
+			System.out.println("Number is Even!");
+		}
+		else {
+			System.out.println("Number is Odd!");
+		}		
+		
+		System.out.println("Program Ended!");
+	}
+}
